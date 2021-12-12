a7028808fd175c75b141268d39bd53ad064a78f7
Rahul Ravi Prakash <eminentrahul@rocketmaiil.com>
2021-12-12 18:24:53 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Rahul Ravi Prakash on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
